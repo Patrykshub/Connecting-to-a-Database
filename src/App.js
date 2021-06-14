f16699457790ab297bb17aa1ec32733a35a11f11
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-06-14 17:08:02 +0100

    feat: sending a post request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 
-import MoviesList from './components/MoviesList';
-import AddMovie from './components/AddMovie';
-import './App.css';
-import './components/Loader.css';
+import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
+import "./App.css";
+import "./components/Loader.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -14,37 +14,50 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch(
+        "https://react-my-burger360-default-rtdb.firebaseio.com/movies.json"
+      );
       if (!response.ok) {
-        throw new Error('Something went wrong!');
+        throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
   }, []);
 
-  useEffect(() => {
-    fetchMoviesHandler();
-  }, [fetchMoviesHandler]);
+  useEffect(() => {}, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://react-my-burger360-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
   }
 
-  let content = <p>Found no movies.</p>;
+  let content = <p>No movies yet.</p>;
 
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
@@ -55,7 +68,14 @@ function App() {
   }
 
   if (isLoading) {
-    content = <div className='lds_ring'><div></div><div></div><div></div><div></div></div>;
+    content = (
+      <div className="lds_ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    );
   }
 
   return (
@@ -64,9 +84,9 @@ function App() {
         <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={() => fetchMoviesHandler()}>Fetch Movies</button>
       </section>
-      <section>{content}</section>
+      <section id="Movies Section">{content}</section>
     </React.Fragment>
   );
 }
--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,3 +1,4 @@
+import { getElementError } from '@testing-library/react';
 import React, { useRef } from 'react';
 
 import classes from './AddMovie.module.css';
@@ -17,23 +18,23 @@ function AddMovie(props) {
       openingText: openingTextRef.current.value,
       releaseDate: releaseDateRef.current.value,
     };
-
+    document.movie_form.reset();
     props.onAddMovie(movie);
   }
 
   return (
-    <form onSubmit={submitHandler}>
+    <form name="movie_form" onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='title'>Title</label>
-        <input type='text' id='title' ref={titleRef} />
+        <input type='text' id='title' name='input' ref={titleRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor='opening-text'>Opening Text</label>
-        <textarea rows='5' id='opening-text' ref={openingTextRef}></textarea>
+        <textarea rows='5' id='opening-text' name='input' ref={openingTextRef}></textarea>
       </div>
       <div className={classes.control}>
         <label htmlFor='date'>Release Date</label>
-        <input type='text' id='date' ref={releaseDateRef} />
+        <input type='text' id='date' name='input' ref={releaseDateRef} />
       </div>
       <button>Add Movie</button>
     </form>
